50cdf3d60254a012293c3ecff0857720069eb150
Edward Jibson <jibson@tuta.io>
2019-05-22 00:22:34 -0700

    Update sido.php

--- src/sido.php
+++ src/sido.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of eddiejibson/sido.
+ *
+ * (c) Edward Jibson <jibson@tuta.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace eddiejibson;
 
 class Sido
